50bcc8b878ce74de95204c9c7e754aaf87147dc8
kindeme <fkounoudji@gmail.com>
2020-12-20 21:51:41 -0600

    destructuring and spreading

--- src/App.js
+++ src/App.js
@@ -5,14 +5,43 @@ import Employee from './Employee';
 
 
 function App() {
+  const employeeInfo = [
+    {
+      firstName: "Jack",
+      lastName: "Smith",
+      age: "25",
+      employeeId: "1"
+    },
+    {
+      firstName: "Stefanie",
+      lastName: "Renne",
+      age: "30",
+      employeeId: "2"
+    },
+    {
+      firstName: "Ryan",
+      lastName: "Gogovor",
+      age: "01",
+      employeeId: "3"
+    },
+    {
+      firstName: "Hillary",
+      lastName: "Smith",
+      age: "25",
+      employeeId: "4"
+    }
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <h1> Company Directory</h1>
-        <Employee firstName ='Jack' lastName= "Smith"/>
-        <Employee firstName ='Stefanie' lastName= "Renne"/>
-        <Employee firstName ='Ryan' lastName= "GOGO"/>
-        <Employee firstName ='Hillary' lastName= "Kounoudji"/>
+        {employeeInfo.map(employee => {
+          
+          return (
+            <Employee key={employee.employeeId} {...employee} />
+          )
+      })}
+
        </header>
     </div>
   );
--- src/Employee.js
+++ src/Employee.js
@@ -1,10 +1,10 @@
 import react from 'react';
 
 const Employee = (props) => {
-    const { firstName, lastName } = props;
+    const { firstName, lastName,age } = props;
     return (
         <h5>
-            Employee Name : {firstName} {lastName}</h5>
+            Employee Name : {firstName} {lastName} with age of : {age}</h5>
     
     )};
 
